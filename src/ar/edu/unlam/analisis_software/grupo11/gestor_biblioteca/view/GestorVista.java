ffc10395707f595d3ad95b14fb75704542ebfa40
anonymus_sheep <seebogado@gmail.com>
2016-06-07 22:07:26 -0300

    Se agrega la interfaz grafica. Falta desacoplar codigo

---
+++ src/ar/edu/unlam/analisis_software/grupo11/gestor_biblioteca/view/GestorVista.java
@@ -0,0 +1,130 @@
+package ar.edu.unlam.analisis_software.grupo11.gestor_biblioteca.view;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import ar.edu.unlam.analisis_software.grupo11.gestor_biblioteca.view.utils.PanelOption;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GestorVista {
+
+	private JFrame frame;
+
+	private PanelOption pnlOptionAltaLibro;
+	private PanelOption pnlOptionModificarLibro;
+	private PanelOption pnlOptionEliminarLibro;
+	private PanelOption pnlOptionConsultas;
+	private PanelOption pnlOptionOrdenarRegistros;
+	private PanelOption pnlOptionListarRegistros;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GestorVista window = new GestorVista();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GestorVista() {
+		initialize();
+		this.inicializarPanelesDeOpciones();
+		this.addPanelToFrame();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.setBounds(100, 100, 1235, 723);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GestorVista.this.cerrarVentana();
+			}
+		});
+		btnSalir.setBounds(568, 619, 117, 25);
+		frame.getContentPane().add(btnSalir);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				GestorVista.this.cerrarVentana();
+				
+			}
+		});	
+		
+	}
+	
+
+	private void addPanelToFrame() {
+		frame.getContentPane().add(this.pnlOptionAltaLibro);
+		frame.getContentPane().add(this.pnlOptionConsultas);
+		frame.getContentPane().add(this.pnlOptionEliminarLibro);
+		frame.getContentPane().add(this.pnlOptionListarRegistros);
+		frame.getContentPane().add(this.pnlOptionModificarLibro);
+		frame.getContentPane().add(this.pnlOptionOrdenarRegistros);
+		
+		
+	}
+
+	public void inicializarPanelesDeOpciones(){
+		//Este es el color que va a utilizar en los paneles de las opciones.
+		Color color = new Color(1, 223, 1);
+		Color colorOnFocus= new Color(195, 198, 195);
+		
+		
+		this.pnlOptionAltaLibro = new PanelOption("", "Altas de libros", color,colorOnFocus );
+		this.pnlOptionAltaLibro.setBounds(168, 78, 229, 170);
+		
+		this.pnlOptionConsultas = new PanelOption("", "Consultar libro",color,colorOnFocus);
+		this.pnlOptionConsultas.setBounds(168, 355, 229, 170);
+		
+		this.pnlOptionEliminarLibro = new PanelOption("", "Eliminar libro", color,colorOnFocus);
+		this.pnlOptionEliminarLibro.setBounds(926, 355, 229, 170);
+		
+		this.pnlOptionListarRegistros = new PanelOption("", "Listar registros", color,colorOnFocus);
+		this.pnlOptionListarRegistros.setBounds(524, 355, 229, 170);
+		
+		this.pnlOptionModificarLibro = new PanelOption("", "Modificar libro", color,colorOnFocus);
+		this.pnlOptionModificarLibro.setBounds(524, 78, 229, 170);
+		
+		this.pnlOptionOrdenarRegistros = new PanelOption("", "Ordenar registros", color,colorOnFocus);
+		this.pnlOptionOrdenarRegistros.setBounds(926, 78, 229, 170);
+		
+		
+	}
+	
+	
+	protected void cerrarVentana() {
+		Integer respuesta = JOptionPane.showConfirmDialog(null, "¿Desea cerrar la aplicacion?");
+		if(respuesta.intValue() == JOptionPane.OK_OPTION){
+			GestorVista.this.frame.dispose();
+		}
+	}
+	
+	
+}
+
+
---
+++ src/ar/edu/unlam/analisis_software/grupo11/gestor_biblioteca/view/utils/PanelOption.java
@@ -0,0 +1,73 @@
+package ar.edu.unlam.analisis_software.grupo11.gestor_biblioteca.view.utils;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class PanelOption extends JPanel implements MouseListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6421483657945768898L;
+	private JLabel lblDescripcionOpcion ;
+	private Color clrFocusLost;
+	private Color clrOnFocus;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelOption(String pathArchivoImagen, String descripcionOpcion, Color color, Color colorOnFocus) {
+		addMouseListener(this);
+		this.clrOnFocus=colorOnFocus;
+		this.clrFocusLost = color ;
+	
+		setLayout(new BorderLayout(0, 0));
+		this.lblDescripcionOpcion = new JLabel(descripcionOpcion);
+		lblDescripcionOpcion.setLabelFor(lblDescripcionOpcion);
+		lblDescripcionOpcion.setHorizontalAlignment(SwingConstants.CENTER);
+		add(lblDescripcionOpcion, BorderLayout.SOUTH);
+		this.setBackground(color);
+		this.setVisible(true);
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		this.setBackground(this.clrOnFocus);
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		this.setBackground(this.clrFocusLost);
+		
+	}
+
+
+}
+
